3fe45285dafc9a0e384f08dc0ee9e25a42e4a2f3
Shivathanu <gcshivathanu@gmail.com>
2021-03-12 13:19:50 +0000

    feat: integrated braintree payment gateway

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -66,3 +66,34 @@ export const listRelated = (productId) => {
 		})
 		.catch((error) => console.log(error));
 };
+
+export const getBraintreeClientToken = (userId, token) => {
+	return fetch(`${API}/braintree/getToken/${userId}`, {
+		method: "GET",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`
+		}
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((error) => console.log(error));
+}
+
+export const processPayment = (userId, token, paymentData) => {
+	return fetch(`${API}/braintree/payment/${userId}`, {
+		method: "POST",
+		headers: {
+			Accept: "application/json",
+			"Content-Type": "application/json",
+			Authorization: `Bearer ${token}`
+		},
+		body: JSON.stringify(paymentData)
+	})
+		.then((response) => {
+			return response.json();
+		})
+		.catch((error) => console.log(error));
+}
